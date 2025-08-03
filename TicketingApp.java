7ac42487ca4c82a49684e6625fff17560da8c5aa
MdRafsanZaman <mrz281@nyu.edu>
2025-08-02 22:07:43 -0400

    Added some basic Panel classes, for some of the panels I suspect we'll need, might remove, rename or replace in the future

--- TicketingApp.java
+++ TicketingApp.java
@@ -1,3 +1,92 @@
 public class TicketingApp {
-    
+
+}
+
+public class MainFrame extends JFrame{
+
+    // List of all screens we'll have (so far only for the User)
+    LoginPanel loginPanel;
+    RegisterPanel registerPanel;
+    EventBrowsePanel browsePanel;
+    EventUpcomingPanel upcomingPanel;
+    PastPurchasePanel previousPanel; // we can also call it EventPastPanel
+
+    TicketPurchasePanel purchasePanel;
+    AccountInfoPanel userinfoPanel;
+
+    public MainFrame(){
+
+        // main screen goes here
+
+    }
+
+}
+
+public class LoginPanel extends JPanel{
+
+    public LoginFrame(){
+
+
+    }
+
+}
+
+public class RegisterPanel extends JPanel{
+
+
+    public RegisterPanel(){
+
+
+    }
+
+}
+
+public class AccountInfoPanel extends JPanel{
+
+    public AccountInfoPanel(){
+
+
+        
+    }
+
+}
+
+public class EventViewerPanel extends JPanel{
+
+    public EventViewerPanel(){
+
+
+
+    }
+
+}
+
+public class EventUpcomingPanel extends JPanel{
+
+    public EventUpcomingPanel(){
+
+
+
+    }
+
+}
+
+public class PastPurchasePanel extends JPanel{
+
+    public PastPurchasePanel(){
+
+
+
+    }
+
+}
+
+public class TicketPurchasePanel extends JPanel{
+
+    public TicketPurchasePanel(){
+
+
+
+    }
+
 }
