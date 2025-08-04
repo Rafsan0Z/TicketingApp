376518f5ccc2f4be858cb297a56e62f7c22142e8
kaizzzed <157258009+kaizzzed@users.noreply.github.com>
2025-08-04 00:05:54 -0400

    testing testing 123

--- TicketingApp/src/MainFrame.java
+++ TicketingApp/src/MainFrame.java
@@ -6,7 +6,7 @@ public class MainFrame extends JFrame {
     LoginPanel loginPanel;
     RegisterPanel registerPanel;
     EventBrowsePanel browsePanel;
-    EventUpcomingPanel upcomingPanel
+    EventUpcomingPanel upcomingPanel;
     PastPurchasePanel previousPanel; // we can also call it EventPastPanel
 
     TicketPurchasePanel purchasePanel;
@@ -15,7 +15,11 @@ public class MainFrame extends JFrame {
     public MainFrame(){
 
         // main screen goes here
-
+        setTitle("Ticketing Application");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
+        setLocationRelativeTo(null); // center the window on the screen
+        setLayout(new CardLayout()); // use CardLayout to switch between panels
     }
 
 
