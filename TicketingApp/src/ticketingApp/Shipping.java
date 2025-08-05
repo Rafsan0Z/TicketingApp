8620036e5e7e98dd2727d2e16be1e8f8b6749546
Sophia Domonoske <swd7169@nyu.edu>
2025-08-05 13:37:59 -0400

    making serialization methods generic

--- TicketingApp/src/ticketingApp/Shipping.java
+++ TicketingApp/src/ticketingApp/Shipping.java
@@ -3,6 +3,7 @@ package ticketingApp;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.function.Function;
 
 public class Shipping implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -10,100 +11,55 @@ public class Shipping implements Serializable{
 	
 	// SERIALIZATION METHODS
 	
-	
-	// SLURPING
-	
 	/**
-	 * This method will imports Venue file
-	 * @throws IOException
+	 * 
+	 * @param <T>
+	 * @param path
+	 * @param newT
+	 * @throws FileNotFoundException
+	 * This method slurps in a file of type T and creates an ArrayList<T>
 	 */
-	public static void slurpVenuesFile() throws IOException{
-		ArrayList<Venue> venueSpreadsheet = new ArrayList<>();
+	public static <T> void slurpFile(String path, Function<String[], T> newT) throws FileNotFoundException {
+		ArrayList<T> spreadsheet = new ArrayList<T>();
 		
-		Scanner kb = new Scanner(new FileReader("src/Venues.csv"));
+		Scanner kb = new Scanner(new FileReader(path));
 		kb.useDelimiter(",");
 		kb.nextLine();
 		while (kb.hasNext()) {
 			String line = kb.nextLine();
-			String[] venueInfo = line.split(",");
-			venueSpreadsheet.add(new Venue(venueInfo));
-		}
-	}
-	/**
-	 * This method will imports the Events file
-	 * @throws IOException
-	 */
-	public static void slurpEventsFile() throws IOException{
-		ArrayList<Event> eventsSpreadsheet = new ArrayList<>();
-		
-		Scanner kb = new Scanner(new FileReader("src/Events.csv"));
-		kb.useDelimiter(",");
-		kb.nextLine();
-		while (kb.hasNext()) {
-			String line = kb.nextLine();
-			String[] eventInfo = line.split(",");
-			eventsSpreadsheet.add(new Event(eventInfo));
+			String[] objectInfo = line.split(",");
+			spreadsheet.add(newT.apply(objectInfo));
 		}
 	}
 	
 	/**
-	 * This method will imports the Tickets file
+	 * 
 	 * @throws IOException
+	 * This method slurps in all necessary files for this project
 	 */
-	public static void slurpTicketsFile() throws IOException{
-		ArrayList<Ticket> ticketsSpreadsheet = new ArrayList<>();
-		
-		Scanner kb = new Scanner(new FileReader("src/Tickets.csv"));
-		kb.useDelimiter(",");
-		kb.nextLine();
-		while (kb.hasNext()) {
-			String line = kb.nextLine();
-			String[] ticketInfo = line.split(",");
-			ticketsSpreadsheet.add(new Ticket(ticketInfo));
-		}
-	}
-	
-	/**
-	 * This method will imports the Users file
-	 * @throws IOException
-	 */
-	public static void slurpUsersFile() throws IOException{
-		ArrayList<User> usersSpreadsheet = new ArrayList<>();
-		
-		Scanner kb = new Scanner(new FileReader("src/Users.csv"));
-		kb.useDelimiter(",");
-		kb.nextLine();
-		while (kb.hasNext()) {
-			String line = kb.nextLine();
-			String[] userInfo = line.split(",");
-			usersSpreadsheet.add(new User(userInfo));
-		}
-	}
-	
 	public static void slurpAll() throws IOException {
-		slurpVenuesFile();
-		slurpEventsFile();
-		slurpTicketsFile();
-		slurpUsersFile();
+		slurpFile("src/Users.csv", User::new);
+		slurpFile("src/Venues.csv", Venue::new);
+		slurpFile("src/Events.csv", Event::new);
+		slurpFile("src/Tickets.csv", Ticket::new);
 	}
 	
 	
-	// DESERIALIZING
 	
 	/**
-	 * This method will deserialize a Venue ArrayList
+	 * This method deserializes an ArrayList<T>
 	 */
 	@SuppressWarnings("unchecked")
-	public static void venuesDeserialization() {
-		String fileName = "venues.ser";
+	public static <T> void deserialization(Serializables s) {
+		String fileName = s.fileName + FILENAME_EXTENSION;
 		
-		ArrayList<Venue> venuesList = new ArrayList<>();
+		ArrayList<T> list = new ArrayList<>();
 		
 		try {
 			FileInputStream fis = new FileInputStream(fileName);
 			ObjectInputStream ois = new ObjectInputStream(fis);
 			
-			venuesList.addAll((ArrayList<Venue>) ois.readObject());
+			list.addAll((ArrayList<T>) ois.readObject());
 			
 			ois.close();
 			fis.close();
@@ -114,94 +70,17 @@ public class Shipping implements Serializable{
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
+		System.out.println("worked i think");
 	}
 	
 	/**
-	 * This method will deserialize an Event ArrayList
-	 */
-	@SuppressWarnings("unchecked")
-	public static void eventsDeserialization() {
-		String fileName = "events.ser";
-		
-		ArrayList<Event> eventsList = new ArrayList<>();
-		
-		try {
-			FileInputStream fis = new FileInputStream(fileName);
-			ObjectInputStream ois = new ObjectInputStream(fis);
-			
-			eventsList.addAll((ArrayList<Event>) ois.readObject());
-			
-			ois.close();
-			fis.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	/**
-	 * This method will deserialize a Ticket ArrayList
-	 */
-	@SuppressWarnings("unchecked")
-	public static void ticketsDeserialization() {
-		String fileName = "tickets.ser";
-		
-		ArrayList<Ticket> ticketsList = new ArrayList<>();
-		
-		try {
-			FileInputStream fis = new FileInputStream(fileName);
-			ObjectInputStream ois = new ObjectInputStream(fis);
-			
-			ticketsList.addAll((ArrayList<Ticket>) ois.readObject());
-			
-			ois.close();
-			fis.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	/**
-	 * This method will deserialize a User ArrayList
-	 */
-	@SuppressWarnings("unchecked")
-	public static void usersDeserialization() {
-		String fileName = "user.ser";
-		
-		ArrayList<User> usersList = new ArrayList<>();
-		
-		try {
-			FileInputStream fis = new FileInputStream(fileName);
-			ObjectInputStream ois = new ObjectInputStream(fis);
-			
-			usersList.addAll((ArrayList<User>) ois.readObject());
-			
-			ois.close();
-			fis.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	// SERIALIZING
-	/**
-	 * This method will serialize a student ArrayList
+	 * 
+	 * @param <T>
 	 * @param variable
 	 * @param s
+	 * This method serializes an ArrayList<T>
 	 */
-	public static void venueSerialization(ArrayList<Venue> variable, Serializables s) {
-		
+	public static <T> void serialization(ArrayList<T> variable, Serializables s) {
 		String fileName = s.fileName + FILENAME_EXTENSION;
 		
 		try {
@@ -218,64 +97,5 @@ public class Shipping implements Serializable{
 			e.printStackTrace();
 		}
 	}
-	
-	public static void eventSerialization(ArrayList<Event> variable, Serializables s) {
-		
-		String fileName = s.fileName + FILENAME_EXTENSION;
-		
-		try {
-			FileOutputStream fos = new FileOutputStream(fileName);
-			ObjectOutputStream oos = new ObjectOutputStream(fos);
-			
-			oos.writeObject(variable);
-			
-			oos.close();
-			fos.close();
-		} catch(FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void ticketSerialization(ArrayList<Ticket> variable, Serializables s) {
-		
-		String fileName = s.fileName + FILENAME_EXTENSION;
-		
-		try {
-			FileOutputStream fos = new FileOutputStream(fileName);
-			ObjectOutputStream oos = new ObjectOutputStream(fos);
-			
-			oos.writeObject(variable);
-			
-			oos.close();
-			fos.close();
-		} catch(FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void studentSerialization(ArrayList<User> variable, Serializables s) {
-		
-		String fileName = s.fileName + FILENAME_EXTENSION;
-		
-		try {
-			FileOutputStream fos = new FileOutputStream(fileName);
-			ObjectOutputStream oos = new ObjectOutputStream(fos);
-			
-			oos.writeObject(variable);
-			
-			oos.close();
-			fos.close();
-		} catch(FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	
 	
 }
