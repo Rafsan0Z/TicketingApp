f5cbd9054f53080d724e1112014fb0f24bb11613
Sophia Domonoske <swd7169@nyu.edu>
2025-08-06 17:41:21 -0400

    add preliminary fields, getters, and setters -- will change later

--- TicketingApp/src/ticketingApp/Ticket.java
+++ TicketingApp/src/ticketingApp/Ticket.java
@@ -2,8 +2,36 @@ package ticketingApp;
 
 public class Ticket {
 
+	private String event;
+	private String venue;
+	private String time;
+	private double cost;
+	
+	public Ticket(String event, String venue, String time, double cost) {
+		this.event = event;
+		this.venue = venue;
+		this.time = time;
+		this.cost = cost;
+	}
+	
 	public Ticket(String[] ticketInfo) {
 		// TODO Auto-generated constructor stub
+	}
+
+	public String getEvent() {
+		return event;
+	}
+
+	public String getVenue() {
+		return venue;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public double getCost() {
+		return cost;
 	}
 
 }
