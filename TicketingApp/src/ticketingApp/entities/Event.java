ddbe5fd6606405096e8274afb1258491c4f33a91
Sophia Domonoske <swd7169@nyu.edu>
2025-08-08 17:54:50 -0400

    Flush out Event class

--- TicketingApp/src/ticketingApp/entities/Event.java
+++ TicketingApp/src/ticketingApp/entities/Event.java
@@ -2,18 +2,22 @@ package ticketingApp.entities;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+import ticketingApp.RegisteredUser;
 
 public class Event {
 	private String name;
 	private Venue venue;
-	private int capacity;
+	private int maxTickets;
 	private Date date;
-	private ArrayList<Ticket> tickets;
+	private List<Ticket> tickets = new ArrayList<Ticket>();
+	private boolean soldOut;
 	
-	public Event(String name, Venue venue, int capacity, Date date) {
+	public Event(String name, Venue venue, int maxTickets, Date date) {
 		this.name = name;
 		this.venue = venue;
-		this.capacity = capacity;
+		this.maxTickets = maxTickets;
 		this.date = date;
 	}
 
@@ -23,8 +27,44 @@ public class Event {
 
 	public String getName() {return name;}
 	public Venue getVenue() {return venue;}
-	public int getCapacity() {return capacity;}
+	public int getMaxTickets() {return maxTickets;}
 	public Date getDate() {return date;}
-	public ArrayList<Ticket> getTickets() {return tickets;}
+	public List<Ticket> getAllTickets() {return tickets;}
+	public List<Ticket> getAvailableTickets() {
+		List<Ticket> availableTickets = new ArrayList<Ticket>();
+		for (Ticket ticket : tickets) {
+			if (!ticket.isSold()) {
+				availableTickets.add(ticket);
+			}
+		}
+		return availableTickets;
+	}
+	public List<Ticket> getPurchasedTickets() {
+		List<Ticket> purchasedTickets = new ArrayList<Ticket>();
+		for (Ticket ticket : tickets) {
+			if (ticket.isSold()) {
+				purchasedTickets.add(ticket);
+			}
+		}
+		return purchasedTickets;
+	}
+	
+	public boolean isSoldOut() {
+		if (getPurchasedTickets().isEmpty()) {
+			return true;
+		}
+		return false;
+	}
+	
+	public void cancelAllTickets() {
+		for (Ticket ticket : tickets) {
+			if (ticket.isSold()) {
+				RegisteredUser user = ticket.getOwner();
+				user.getCurrentTickets().remove(ticket);
+			}
+		}
+		
+		tickets.removeAll(tickets);
+	}
 
 }
